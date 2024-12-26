5c763d01c7807206e72eb4e7074edbf4844d9487
Nishant Kumar Bhadani <kumarbhadanin@gmail.com>
2024-12-26 10:16:26 +0530

    LocalStorage With ContextApi todo Project

---
+++ TodoProject/src/Contexts/index.js
@@ -0,0 +1,10 @@
+export {TodoContext, useTodo,TodoProvider} from "./TodoContext"
+
+// // import TodoContext from "./TodoContext";
+// import { useTodo } from "./TodoContext";
+// import { TodoProvider } from "./TodoContext";
+
+// export {
+//     // TodoContext,
+//     useTodo,
+//     TodoProvider
